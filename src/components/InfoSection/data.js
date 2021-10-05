ceaebf1d271cd66c190da2a8c1db21edb8f4ae39
sachira dilthushka <sachiradilthushka@gmail.com>
2021-10-05 10:50:26 +0530

    about section

---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,18 @@
+
+export const homeObjectOne ={
+  id: 'about',
+  lightBg: false,
+  lightText:true,
+  lightTextDesc:true,
+  topLine: 'Software Company',
+  headline: 'Professional Businesses know the importance of Bespoke Software which help reach greater heights',
+  description: 'Bridging the technology gap whilst bringing the world together with cutting-edge web & mobile software solutions whilst creating & enhancing the key stakeholder value',
+  buttonLabel: 'Get started',
+  imgStart: false,
+  img: require('../../images/svg-1.svg'),
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkTet: false,
+
+}
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Button } from 'react-scroll';
+import { Colum2, ImgWrap, InfoContainer, InfoRow, Colum1, TextWrapper, TopLine, Heading, Subtitle,Btnwrap, Img  } from './infoElements';
+
 
 const InfoSection = () => {
 	return (
 		<>
 		<InfoContainer>
-			
+		  <InfoRow>
+			<Colum1>
+			  <TextWrapper>
+				  <TopLine>TopLine</TopLine>
+				  <Heading>Heading</Heading>
+				  <Subtitle>Subtitle</Subtitle>
+				  <Btnwrap>
+					  <Button to ='home'/>
+				  </Btnwrap>
+			  </TextWrapper>
+			</Colum1>
+
+			<Colum2>
+			 <ImgWrap>
+			  <Img/>
+			 </ImgWrap>
+			</Colum2>  
+		   </InfoRow>	
 		</InfoContainer>	
 		</>
 	)
